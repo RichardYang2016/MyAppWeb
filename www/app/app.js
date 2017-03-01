a51bf25413104a756ce99ce5a78d8870229e8a87
Richard Z <{yangzhengcn@gmail.com}>
2017-02-28 22:47:57 -0800

    All signup/in

---
+++ www/app/app.js
@@ -0,0 +1,20 @@
+
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
+
+  .run(function ($ionicPlatform, appService) {
+    firebase.initializeApp(appService.getFirebaseConfig());
+    $ionicPlatform.ready(function () {
+
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        // $cordovaKeyboard.hideAccessoryBar(true);
+        // cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
+  }).run(function () {
+       
+  })
---
+++ www/app/core/services.js
@@ -0,0 +1,103 @@
+(function () {
+
+    'use strict'
+
+    angular.module('app.services', [])
+
+        .service('appService', ['$state', '$ionicPopup', '$ionicActionSheet', '$ionicHistory', '$ionicLoading', '$timeout', '$http',
+            function ($state, $ionicPopup, $ionicActionSheet, $ionicHistory, $ionicLoading, $timeout, $http) {
+                //global data                
+                var currentUser;
+
+
+
+
+                //service logic
+                return {
+                    getFirebaseConfig: function () {
+                        return {
+                            apiKey: "AIzaSyBxuKImRdb7-VRScgwZYjpfEmc9sgBSlqg",
+                            authDomain: "agileacc-1488095635682.firebaseapp.com",
+                            databaseURL: "https://agileacc-1488095635682.firebaseio.com/",
+                            storageBucket: "agileacc-1488095635682.appspot.com"
+                            // messagingSenderId: "<SENDER_ID>",
+                        };
+
+                    },
+                    setUserInfo: function (user) {
+                        currentUser = user;
+                    },
+                    getUserInfo: function (user) {
+                        return currentUser;
+                    },
+                    Loading: function (params) {
+                        if (params === 'show') {
+                            $ionicLoading.show({
+                                template: '<ion-spinner></ion-spinner>'
+                            });
+                        } else {
+                            $ionicLoading.hide();
+                        }
+                    },
+                    KeepKeyboardOpen: function (params) {
+                        var txtInput = angular.element(document.body.querySelector(params));
+                        txtInput.one('blur', function () {
+                            txtInput[0].focus();
+                        });
+                    },
+                    showAlert: function (title, text, buttonText, buttonType, page) {
+                        var alertPopup = $ionicPopup.alert({
+                            title: title,
+                            template: text,
+                            buttons: [{ text: buttonText, type: buttonType }]
+                        });
+                        $timeout(function () {
+                            alertPopup.close();
+                        }, 1500000);
+
+                        alertPopup.then(function (res) {
+                            page != null ? $state.go(page) : '';
+                            alertPopup.close();
+                        });
+                    },
+                    getGreetingTime: function (m) {
+                        var g = null;
+
+                        if (!m || !m.isValid()) { return; } //if we can't find a valid or filled moment, we return.
+
+                        var split_afternoon = 12 //24hr time to split the afternoon
+                        var split_evening = 17 //24hr time to split the evening
+                        var currentHour = parseFloat(m.format("HH"));
+
+                        if (currentHour >= split_afternoon && currentHour <= split_evening) {
+                            g = "Good Afternoon";
+                        } else if (currentHour >= split_evening) {
+                            g = "Good Evening";
+                        } else {
+                            g = "Good Morning";
+                        }
+                        return g;
+                    },
+                    getCameraOptions: function () {
+                        return {
+                            quality: 80,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: Camera.PictureSourceType.CAMERA,
+                            encodingType: Camera.EncodingType.JPEG,
+                            saveToPhotoAlbum: true,
+                            correctOrientation: true,
+                        };
+                    },
+                    getLibraryOptions: function () {
+                        return {
+                            quality: 80,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+                            encodingType: Camera.EncodingType.JPEG,
+                            saveToPhotoAlbum: false,
+                            correctOrientation: true,
+                        };
+                    },
+                }
+            }]);
+})();
